e6d5a06998b240412344b00fe120f0adcae80593
kozyrev42 <kozyrev054@gmail.com>
2022-10-09 09:57:38 +0700

    мелкие правки

--- example1/index.php
+++ example1/index.php
@@ -1,9 +1,11 @@
 <?
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
-$APPLICATION->SetPageProperty("ROBOTS", "index, follow+ (Свойства страницы)");
-$APPLICATION->SetPageProperty("TITLE", "Title+ (Свойства страницы)");
+/* далее следуют свойства страницы, доступные для редактирования из админки */
+$APPLICATION->SetPageProperty("ROBOTS", "index, follow+++ (Свойства страницы)");
+$APPLICATION->SetPageProperty("TITLE", "Title+++ (Свойства страницы)");
 $APPLICATION->SetPageProperty("keywords", "ключевые слова example1+ (Свойства страницы)");
 $APPLICATION->SetPageProperty("description", "описание из админки example1+ (Свойства страницы)");
+/* заголовок страницы */
 $APPLICATION->SetTitle("Title - который по умолчанию");     /* Строка будет отображаться в Логической структуре сайта */
 ?>
 hello world !
